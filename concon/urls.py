375acaba0b5172e758b0fcbe0883bd740c4a1741
Kay Cha <kay@hannal.net>
2016-09-07 21:52:25 +0900

    퀴즈 목록 화면 추가

--- concon/urls.py
+++ concon/urls.py
@@ -2,9 +2,11 @@ from django.conf.urls import url
 from django.contrib import admin
 
 from quiz.views import toppage
+from quiz.views import list_quiz
 
 
 urlpatterns = [
+    url(r'^list/$', list_quiz),  # /list/
     url(r'^$', toppage),
     url(r'^admin/', admin.site.urls),
 ]
--- quiz/views.py
+++ quiz/views.py
@@ -7,3 +7,23 @@ def toppage(request):
         'welcome_message': '환영합니다. 퀴즈를 즐겨보세요.',
     }
     return render(request, 'toppage.html', ctx)
+
+
+def list_quiz(request):
+    quizzes = [
+        {
+            'image': '404.png',
+            'title': '첫 번째 퀴즈',
+            'message': '퀴즈 소개 메시지',
+        },
+        {
+            'image': '404.png',
+            'title': '두 번째 퀴즈',
+            'message': '2번 퀴즈 소개 메시지',
+        },
+    ]
+
+    ctx = {
+        'quizzes': quizzes,
+    }
+    return render(request, 'list_quiz.html', ctx)
