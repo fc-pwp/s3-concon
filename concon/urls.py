740805bf92893778605722cec38ebbb3a23ba8d9
Kay Cha <kay@hannal.net>
2016-09-07 23:08:10 +0900

    url 패턴에 이름 붙임

--- concon/urls.py
+++ concon/urls.py
@@ -7,7 +7,7 @@ from quiz.views import start_quiz
 
 
 urlpatterns = [
-    url(r'^quiz/([0-9]+)/$', start_quiz), # /quiz/1/
+    url(r'^quiz/(?P<pk>[0-9]+)/$', start_quiz), # /quiz/1/
     # url(r'^list/$', list_quiz),  # /list/
     url(r'^$', list_quiz),
     # url(r'^$', toppage),
