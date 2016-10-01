f2bec921e888d8dbec57e509d41d0c98eb1266f0
Kay Cha <kay@hannal.net>
2016-10-01 10:36:41 +0900

    QuestionAdmin 추가

--- quiz/admin.py
+++ quiz/admin.py
@@ -1,9 +1,21 @@
 from django.contrib import admin
 
 from .models import Quiz
+from .models import Question
 
 
 class QuizAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['id', 'title', 'created', 'updated']
+    list_display_links = ['id', 'title', ]
+    search_fields = ['title']
 
 admin.site.register(Quiz, QuizAdmin)
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'created', 'updated']
+    list_display_links = ['id', 'title', ]
+    search_fields = ['title']
+    list_filter = ['quiz', 'sequence']
+
+admin.site.register(Question, QuestionAdmin)
